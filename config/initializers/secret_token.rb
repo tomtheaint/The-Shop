75f2c46d775c4802aa429a58ecf133a00a5431ee
Tommy Theis <tomtheis@gmail.com>
2012-01-30 15:17:18 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheShop::Application.config.secret_token = 'd24d1c7f07e102496c1de05f45736e05ef4a082fbce0561d003d2af4774bf1d87978bba8823bf5c5ff0810add8219efd242ebcaca0604178242e86012a1ea981'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TheShop::Application.config.session_store :cookie_store, :key => '_The-Shop_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TheShop::Application.config.session_store :active_record_store
